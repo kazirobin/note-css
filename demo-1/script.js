7c88094d8f56f8399e4d7b9965d9d6332c01ecf2
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-10-19 22:48:44 +0600

    ok

---
+++ demo-1/script.js
@@ -0,0 +1,14 @@
+const main = document.getElementsByTagName('main')[0]
+const nav = document.getElementsByTagName('nav')[0]
+const aside = document.getElementsByTagName('aside')[0]
+
+const chat =[
+    {
+        id:'1',
+        img:"http://img.com",
+        username:"name",
+        mgs:[
+            "msg1",'msg2'
+        ]
+    }
+]
