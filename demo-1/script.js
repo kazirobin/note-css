e708215488e5535609fe52d0048e785b85af977d
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-10-20 07:06:39 +0600

    ok

--- demo-1/script.js
+++ demo-1/script.js
@@ -1,14 +1,26 @@
 const main = document.getElementsByTagName('main')[0]
 const nav = document.getElementsByTagName('nav')[0]
 const aside = document.getElementsByTagName('aside')[0]
+// Select all user elements
+const users = document.querySelectorAll('.user');
 
-const chat =[
-    {
-        id:'1',
-        img:"http://img.com",
-        username:"name",
-        mgs:[
-            "msg1",'msg2'
-        ]
-    }
-]
+// Add click event listener to each user element
+users.forEach((user) => {
+  user.addEventListener('click', () => {
+   main.classList.toggle('active')
+  });
+});
+
+// Select all title elements inside .user
+const titles = document.querySelectorAll('.user .title');
+
+// Add click event listener to each title element
+titles.forEach((title) => {
+  title.addEventListener('click', (event) => {
+    // Show an alert when the title is clicked
+    alert('Title clicked!');
+    
+    // Stop the event from bubbling up to the user div
+    event.stopPropagation();
+  });
+});
